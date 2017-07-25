838bf03fb7138879f43d756c29869762c9b598db
mdaehn <markus.daehn@gmail.com>
2017-07-25 16:56:27 -0700

    added user and drawer actions and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
-// import { combineReducers } from 'redux';
-// import { reducer as formReducer } from 'redux-from';
-// import drawer from './drawer';
-// import user from './user';
-//
-//
-// export default combineReducers({
-//   form: formReducer,
-//   drawer,
-//   user
-// });
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-from';
+import drawer from './drawer';
+import user from './user';
+
+
+export default combineReducers({
+  form: formReducer,
+  drawer,
+  user
+});
