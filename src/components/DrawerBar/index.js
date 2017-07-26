7202ad48841aa4f14fb27958f2fd90828f18454f
mdaehn <markus.daehn@gmail.com>
2017-07-25 22:36:03 -0700

    added the drawer component

---
+++ src/components/DrawerBar/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Image, TouchableOpacity } from 'react-native';
+import {
+  Text,
+  Container,
+  List,
+  ListItem,
+  Content
+} from 'native-base';
+
+const routes = ['Home', 'BlankPage2'];
+export default class DrawerBar extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+  render() {
+    <Container>
+      <Content>
+        <Image
+            source={{
+              uri:'https://github.com/GeekyAnts/NativeBase-KitchenSink/raw/react-navigation/img/drawer-cover.png'
+            }}
+            style={{
+              height: 120,
+              alignSelf: 'stretch',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+          <TouchableOpacity
+            style={{
+              height 120,
+              alignSelf: 'stretch',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+            onPress={() => this.props.navigation.navigate('DrawerClose')}
+          >
+          <Image
+              square
+              style={{ height:80, width:70 }}
+              source={{
+                uri: 'https://github.com/GeekyAnts/NativeBase-KitchenSink/raw/react-navigation/img/logo.png'
+              }}
+            />
+          </TouchableOpacity>
+        </Image>
+        <List>
+          dataArray={routes}
+          renderRow={data => {
+            return (
+              <ListItem
+                button
+                onPress={() => this.props.navigation.navigatee(data)}
+              >
+                <Text>{data}</Text>
+              </ListItem>
+            );
+          }}
+        />
+      </Content>
+    </Container>
+  }
+}
