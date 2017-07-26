20dafd4aa15176e910c835bb399af626dd4f9e3d
mdaehn <markus.daehn@gmail.com>
2017-07-25 17:01:44 -0700

    added configureStore

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,23 +1,23 @@
-//
-// import { AsyncStorage } from 'react-native';
-// import devTools from 'remote-redux-devtools';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import { persistStore } from 'redux-persist';
-// import reducer from './reducers';
-// //TODO: Replace with redux-promise
-// import promise from './promise';
-//
-// export default function configureStore(onCompletion:()=>void):any {
-//   const enhancer = compose(
-//     applyMiddleware(thunk, promise),
-//     devTools({
-//       name: 'yuzsa', realtime: true,
-//     }),
-//   );
-//
-//   const store = createStore(reducer, enhancer);
-//   persistStore(store, { storage: AsyncStorage }, onCompletion);
-//
-//   return store;
-// }
+
+import { AsyncStorage } from 'react-native';
+import devTools from 'remote-redux-devtools';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist';
+import reducer from './reducers';
+//TODO: Replace with redux-promise
+import promise from './promise';
+
+export default function configureStore(onCompletion:()=>void):any {
+  const enhancer = compose(
+    applyMiddleware(thunk, promise),
+    devTools({
+      name: 'yuzsa', realtime: true,
+    }),
+  );
+
+  const store = createStore(reducer, enhancer);
+  persistStore(store, { storage: AsyncStorage }, onCompletion);
+
+  return store;
+}
