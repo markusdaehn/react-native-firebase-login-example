825abb7efff2be853a27b0d7306faa88bf309252
mdaehn <markus.daehn@gmail.com>
2017-07-30 10:15:36 -0700

    updated login background with logo

--- app/screens/Login/index.js
+++ app/screens/Login/index.js
@@ -16,17 +16,17 @@ import { setUser } from '../../actions/user';
 import styles from './styles';
 import LoginForm from './LoginForm';
 
-const background = require('../../../images/shadow.png');
+const background = require('../../../images/login-bg.png');
 
 export default class Login extends Component {
   render() {
-    const navigate = this.props.navigation.navigate.bind(this.props.navigation);
+    const navigate = () => this.props.navigation.navigate('Home');
     return (
       <Container>
         <View style={styles.container}>
           <Content>
             <Image source={background} style={styles.shadow}>
-              <LoginForm login={() => navigate('Home')}/>
+              <LoginForm login={navigate}/>
             </Image>
           </Content>
         </View>
