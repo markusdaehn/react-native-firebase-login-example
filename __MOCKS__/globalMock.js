c88bb76a3f8f4935a6da909165219728523dccb7
mdaehn <markus.daehn@gmail.com>
2017-08-18 16:53:06 -0700

    upgraded react-native and related dependencies

--- __MOCKS__/globalMock.js
+++ __MOCKS__/globalMock.js
@@ -1,6 +1,1 @@
-//require('react-native-mock/mock');
-// import { fetch } from 'isomorphic-fetch' // from a dependency node module that I spoke of in the previous solution.
-//
-// global.fetch = fetch
-// global.mapPropsToStyleNames = global.mapPropsToStyleNames || {}
-// global.window = global.window || {}
+require('react-native-mock/mock');
